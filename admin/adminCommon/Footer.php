f110480899ad00d918ec69628f42b45639ea860f
minhnnhaui@gmail.com <minhnnhaui@gmail.com>
2024-04-14 12:44:07 +0700

    sửa title

--- admin/adminCommon/Footer.php
+++ admin/adminCommon/Footer.php
@@ -1,5 +1,5 @@
 <div class="footer text-right">
     <div class="container p-2 flex-center justify-right py-4">
-        <p class="m-0">Group8-TTCN</p>
+        <p class="m-0">TTCN_Group8</p>
     </div>
 </div>
